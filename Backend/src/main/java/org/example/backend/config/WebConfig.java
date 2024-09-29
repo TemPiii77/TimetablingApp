88c4273a5fa0665170db17023076965f6f7b0bf6
Toth-Andor Kristof <t.a.kristof0210@gmail.com>
2024-09-29 11:59:59 +0100

    Enabling Cross-Origin Resource Sharing

---
+++ Backend/src/main/java/org/example/backend/config/WebConfig.java
@@ -0,0 +1,26 @@
+package org.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+    }
+}
